3b2c08e2251db90a2218a2b3830f6500aad49838
Einar Lielmanis <einars@gmail.com>
2012-03-31 02:22:32 +0300

    Proper dinging

--- tomate.py
+++ tomate.py
@@ -16,7 +16,7 @@ GREEN_AFTER = 60 * 44
 
 class Pomodoro:
     def __init__(s):
-        s.icon=gtk.status_icon_new_from_file(s.icon_directory() + "idle.png")
+        s.icon=gtk.status_icon_new_from_file(s.resource("idle.png"))
         s.icon.set_tooltip("Idle")
         s.state = "idle"
         s.tick_interval = 5 # number of seconds between each poll
@@ -36,16 +36,16 @@ class Pomodoro:
         s.work_started = time()
         s.phase = 1
         s.state = "working"
-        s.icon.set_from_file(s.icon_directory() + "phase-1.png")
+        s.icon.set_from_file(s.resource("phase-1.png"))
 
     def start_idling(s):
         s.state = "idle"
-        s.icon.set_from_file(s.icon_directory() + "idle.png")
+        s.icon.set_from_file(s.resource("idle.png"))
         s.icon.set_tooltip("Worked for %s." % s.format_time(time() - s.work_started))
 
 
-    def icon_directory(s):
-        return os.path.dirname(os.path.realpath(__file__)) + os.path.sep
+    def resource(s, file_name):
+        return os.path.dirname(os.path.realpath(__file__)) + os.path.sep + file_name
 
     def icon_click(s, _):
         delta = time() - s.work_started
@@ -53,6 +53,9 @@ class Pomodoro:
             s.start_work()
         else:
             s.start_idling()
+
+    def ding(s):
+        os.system('aplay %s' % s.resource('ding.wav'))
 
     def update(s):
         if s.state == "working":
@@ -68,18 +71,14 @@ class Pomodoro:
 
             if s.phase != current_phase:
                 s.phase = current_phase
-                s.icon.set_from_file( "%sphase-%d.png" % (s.icon_directory(), current_phase))
+                s.icon.set_from_file( s.resource("phase-%d.png" % current_phase) )
                 if current_phase == 3:
-                    os.system('aplay ding.wav')
+                    gobject.timeout_add(300, s.ding)
 
         gobject.timeout_add(s.tick_interval*1000, s.update)
 
     def main(s):
-        # All PyGTK applications must have a gtk.main(). Control ends here
-        # and waits for an event to occur (like a key press or mouse event).
         gtk.main()
 
-# If the program is run directly or passed as an argument to the python
-# interpreter then create a Pomodoro instance and show it
 if __name__ == "__main__":
     Pomodoro().main()
