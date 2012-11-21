f5d21914779f4e68b871248651c8590fe18afe7a
Einar Lielmanis <einars@gmail.com>
2012-11-21 23:10:55 +0200

    set-skype-status, repeated events at 49/50 minutes

--- tomate.py
+++ tomate.py
@@ -9,20 +9,21 @@ from math import floor
 gtk.gdk.threads_init()
 import gobject
 
-YELLOW_AFTER = 60 * 9
-GREEN_AFTER = 60 * 44
-#YELLOW_AFTER = 4
-#GREEN_AFTER = 9
+YELLOW_AFTER = 60 * 24
+GREEN_AFTER = 60 * 25
+REYELLOW_AFTER = 60 * 49
+OFF_AFTER = 60 * 50
 
 class Pomodoro:
     def __init__(s):
-        s.icon=gtk.status_icon_new_from_file(s.resource("idle.png"))
+        s.icon=gtk.status_icon_new_from_file(s.resource("phase-0.png"))
         s.icon.set_tooltip("Idle")
         s.state = "idle"
         s.tick_interval = 5 # number of seconds between each poll
         s.icon.connect('activate', s.icon_click)
         s.icon.set_visible(True)
         s.work_started = 0
+        gobject.timeout_add(200, s.ding)
         s.update()
 
     def format_time(s, seconds):
@@ -40,7 +41,8 @@ class Pomodoro:
 
     def start_idling(s):
         s.state = "idle"
-        s.icon.set_from_file(s.resource("idle.png"))
+        os.system('./set-skype-status online')
+        s.icon.set_from_file(s.resource("phase-0.png"))
         s.icon.set_tooltip("Worked for %s." % s.format_time(time() - s.work_started))
 
 
@@ -50,12 +52,19 @@ class Pomodoro:
     def icon_click(s, _):
         delta = time() - s.work_started
         if s.state == "idle":
+            os.system('./set-skype-status invisible')
             s.start_work()
         else:
+            os.system('./set-skype-status online')
             s.start_idling()
 
-    def ding(s):
-        os.system('aplay %s' % s.resource('ding.wav'))
+    def triple_ding(s):
+        s.ding()
+        s.ding()
+        s.ding()
+
+    def ding(s, times = 1):
+        os.system('aplay alarm.wav')
 
     def update(s):
         if s.state == "working":
@@ -67,13 +76,19 @@ class Pomodoro:
                 current_phase = 2
             if delta > GREEN_AFTER:
                 current_phase = 3
-
-
-            if s.phase != current_phase:
-                s.phase = current_phase
-                s.icon.set_from_file( s.resource("phase-%d.png" % current_phase) )
-                if current_phase == 3:
-                    gobject.timeout_add(300, s.ding)
+            if delta > REYELLOW_AFTER:
+                current_phase = 4
+            if delta > OFF_AFTER:
+                os.system('./set-skype-status online')
+                s.icon.set_from_file( s.resource("phase-%d.png" % 0) )
+                s.state = "idle"
+                gobject.timeout_add(200, s.triple_ding)
+            else:
+                if s.phase != current_phase:
+                    s.phase = current_phase
+                    s.icon.set_from_file( s.resource("phase-%d.png" % current_phase) )
+                    if current_phase == 3:
+                        gobject.timeout_add(200, s.ding)
 
         gobject.timeout_add(s.tick_interval*1000, s.update)
 
